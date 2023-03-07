2baa9ead1590103594aa5093dc770c6d4a8fead6
K-DoYeon <loveworud9@gmail.com>
2023-03-07 11:34:19 +0900

    first commit

---
+++ 0127/sc.js
@@ -0,0 +1,50 @@
+//var box;
+//box = 100;
+//box = 30;
+//var str = "문자형 변수";
+//
+//document.write(box,str);
+//document.write("문자를 쓸때는 따옴표 찍자");
+//document.write("2000");
+//
+//var num = 100; //숫자형
+//var num2 = "100"; //문자형
+//var num3 = num + num2;
+//document.write(num3);
+//
+//숫자형, 문자형, 논리형
+//var s = true;
+//document.write(s);
+//
+//논리형
+//var t = 10;
+//var u - 100;
+//document.write(t > u);
+//
+////0 은 false 1 true 값이 있으면 true,var -'';은 null / var o;은 undefined
+//
+//var o = 1;
+//document.write(Boolean(o));
+//
+//var a, b, c, d;
+//
+//a = true;
+//b = false;
+//c = 10 > 5;
+//d = Boolean(null);
+//
+//document.write(a, "<br>");
+//document.write(b, "<br>");
+//document.write(c, "<br>");
+//document.write(d, "<br>");
+//
+////typeof 변수 또는 데어터 - 자료형을 확인 할 수 있다
+
+
+var num = 100;
+var str = "자바스크립트";
+
+
+//document.write 는 visual studio code만 먹힘
+document.write(typeof num, "<br>");
+document.write(typeof str);
